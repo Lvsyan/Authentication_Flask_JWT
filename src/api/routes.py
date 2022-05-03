a77409e392be52d02df2d24aab1458fd5fb22348
Lvsyan <Lvsyan96@gmail.com>
2022-05-03 19:31:03 +0000

    Cambios inicio

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,11 +8,9 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route("/register", methods=["POST"])
+def register_user():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    if body_email and body_password:
+        new_user
---
+++ src/front/js/pages/register.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const Register = () => {
+  const [user, setUser] = useState({});
+
+  const sendUserInfo = async () => {
+    const response = await fetch(
+      "https://3001-4geeksacade-reactflaskh-xywfzrlnq5x.ws-eu43.gitpod.io/"
+    );
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <div className="row">
+        <label htmlFor="email" className="col-1">
+          Email
+        </label>
+        <input
+          id="email"
+          className="col-3"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        ></input>
+        <label htmlFor="password" className="col-1">
+          Password
+        </label>
+        <input
+          id="password"
+          className="col-3"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        ></input>
+        <button className="col-2 offset-1" onClick={() => sendUserInfo()}>
+          Register new user
+        </button>
+      </div>
+    </div>
+  );
+};
